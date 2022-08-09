00cd8333dc13cc168bc1c1d1ac268ded16728d2a
Эван Крошкин <>
2022-08-09 09:57:35 +0300

    Initial project setup

---
+++ Marvel/Marvel/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  Marvel
+//
+//  Created by Эван Крошкин on 9.08.22.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
---
+++ Marvel/Marvel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marvel
+//
+//  Created by Эван Крошкин on 9.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
